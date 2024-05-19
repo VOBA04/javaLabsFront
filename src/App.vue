376372adaed5307aed00618aaccc084d6317ec51
VOBA04 <vova.bolashenko@gmail.com>
2024-05-19 17:30:26 +0300

    1

--- src/App.vue
+++ src/App.vue
@@ -256,6 +256,7 @@ export default {
 
 <template>
     <h1>Погода</h1>
+    
     <div class="box">
         <input v-show="this.selectedUser != 'admin'" type="text" v-model="cityName" @keyup.enter="getWeather()"
             placeholder="Введите город">
